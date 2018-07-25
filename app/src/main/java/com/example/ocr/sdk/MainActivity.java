1669ba8f0b6044d3e99e185decc8555f3e823c14
472184081@qq.com <472184081@qq.com>
2018-07-25 17:36:42 +0800

    优化照片存储路径

--- app/src/main/java/com/example/ocr/sdk/MainActivity.java
+++ app/src/main/java/com/example/ocr/sdk/MainActivity.java
@@ -3,11 +3,11 @@ package com.example.ocr.sdk;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -27,6 +27,7 @@ import com.alibaba.cloudapi.sdk.model.ApiRequest;
 import com.alibaba.cloudapi.sdk.model.ApiResponse;
 import com.example.ocr.BuildConfig;
 import com.example.ocr.R;
+import com.example.ocr.sdk.utils.FileUtil;
 import com.example.ocr.sdk.utils.LuBanUtils;
 import com.ocr.aliocrlibrary.common.EnumOcrFace;
 import com.ocr.aliocrlibrary.http.OcrApi;
@@ -46,6 +47,7 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final String DEFAULT_DISK_CACHE_DIR = "luban_disk_cache";
     private final int REQUEST_CODE_TAKE_PHOTO = 1001;
     private final int FACE_CROP_IMG_CODE = 1002;
     private final int MY_PERMISSIONS_REQUEST = 10023;
@@ -65,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
 
         //需要初始化阿里身份证识别的key
         OcrApi.getInstance().setKey("你的appKey", "你的AppSecret");
+
 
 
         mIV = findViewById(R.id.mIV);
@@ -96,6 +99,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * 6.0以上检查权限
+     */
     private void checkAllPermission() {
         boolean needApply = false;
         for (String permission : allPermissions) {
@@ -161,15 +167,23 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
+    /**
+     * 调用开启Camera
+     */
     private void openCamera() {
         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
 
         if (intent.resolveActivity(getPackageManager()) != null) {
-            String filename = new SimpleDateFormat("yyyyMMdd-HHmmss", Locale.CHINA)
-                    .format(new Date()) + ".png";
-            File file = new File(Environment.getExternalStorageDirectory(), filename);
+            String filename = "original_"
+                    + new SimpleDateFormat("yyyyMMdd-HHmmss", Locale.CHINA).format(new Date())
+                    + ".png";
+//            File file = new File(Environment.getExternalStorageDirectory(), filename);
+            // Android/data/com.example.ocr/cache/luban_disk_cache/
+            File file = new File(FileUtil.getImageCacheDir(this, DEFAULT_DISK_CACHE_DIR), filename);
             mCurrentPhotoPath = file.getAbsolutePath();
+            Log.i(TAG, "原始照片地址:" + mCurrentPhotoPath);
 
+            //Android7.0以上uri不能直接去获取
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 fileUri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".fileProvider", file);
             } else {
@@ -177,53 +191,70 @@ public class MainActivity extends AppCompatActivity {
             }
             intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
             startActivityForResult(intent, REQUEST_CODE_TAKE_PHOTO);
-            Log.i(TAG, "take pic");
+            Log.i(TAG, "开启拍照");
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE_TAKE_PHOTO && resultCode == RESULT_OK) {
-            mIV.setImageBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath));
-            Log.i(TAG, "onActivityResult photo");
+        switch (requestCode) {
+            case REQUEST_CODE_TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    onResultCamera();
+                }
+                break;
+            case FACE_CROP_IMG_CODE:
+                onResultCrop(data);
+                break;
+        }
 
-            Uri outputUri = null;
-            try {
-                outputUri = Uri.fromFile(File.createTempFile("corp1", ".jpg"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    }
+
+    private void onResultCamera() {
+        Log.i(TAG, "获取到拍照后的回调");
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
+        mIV.setImageBitmap(bitmap);
+
+        try {
             //裁剪图片
+            File imageCacheDir = FileUtil.getImageCacheDir(this, DEFAULT_DISK_CACHE_DIR);
+            File tempFile = File.createTempFile("crop_", ".jpg", imageCacheDir);
+            Log.i(TAG, "裁剪图片地址:" + tempFile.getAbsolutePath());
+            Uri outputUri = Uri.fromFile(tempFile);
             Crop.of(fileUri, outputUri).withAspect(18, 11).start(this, FACE_CROP_IMG_CODE);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
-        if (requestCode == FACE_CROP_IMG_CODE) {
-            Log.i(TAG, "onActivityResult photo crop");
-            new Thread(
-                    new Runnable() {
-                        @Override
-                        public void run() {
-                            //压缩图片
-                            LuBanUtils.compressImg(MainActivity.this, Crop.getOutput(data), new LuBanUtils.OnMyCompressListener() {
-                                @Override
-                                public void onSuccess(File file) {
-                                    Log.i(TAG, "compressImg success");
-                                    mCropIV.setImageBitmap(BitmapFactory.decodeFile(file.getAbsolutePath()));
-                                    getOcrResult(file, face);
-                                }
+    private void onResultCrop(final Intent data) {
+        Log.i(TAG, "获取到裁剪后的回调");
 
-                                @Override
-                                public void onError(Throwable e) {
-                                    Log.i(TAG, "compressImg failed");
-                                }
-                            });
-                        }
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        //压缩图片
+                        LuBanUtils.compressImg(MainActivity.this, Crop.getOutput(data), new LuBanUtils.OnMyCompressListener() {
+                            @Override
+                            public void onSuccess(File file) {
+                                Log.i(TAG, "裁剪 success");
+                                String absolutePath = file.getAbsolutePath();
+                                mCropIV.setImageBitmap(BitmapFactory.decodeFile(absolutePath));
+                                Log.i(TAG, "压缩图片地址:" + absolutePath);
+                                getOcrResult(file, face);
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                Log.i(TAG, "裁剪 failed");
+                            }
+                        });
                     }
-            ).start();
-
-        }
-
+                }
+        ).start();
     }
 
     private void getOcrResult(File file, EnumOcrFace face) {
--- app/src/main/java/com/example/ocr/sdk/utils/FileUtil.java
+++ app/src/main/java/com/example/ocr/sdk/utils/FileUtil.java
@@ -4,12 +4,36 @@
 package com.example.ocr.sdk.utils;
 
 import android.content.Context;
+import android.support.annotation.Nullable;
 
 import java.io.File;
 
 public class FileUtil {
+
     public static File getSaveFile(Context context) {
         File file = new File(context.getFilesDir(), "pic.jpg");
         return file;
     }
+
+    /**
+     * Returns a directory with the given name in the private cache directory of the application to
+     * use to store retrieved media and thumbnails.
+     *
+     * @param context   A context.
+     * @param cacheName The name of the subdirectory in which to store the cache.
+     */
+    @Nullable
+    public static File getImageCacheDir(Context context, String cacheName) {
+        File cacheDir = context.getExternalCacheDir();
+        if (cacheDir != null) {
+            File result = new File(cacheDir, cacheName);
+            if (!result.mkdirs() && (!result.exists() || !result.isDirectory())) {
+                // File wasn't able to create a directory, or the result exists but not a directory
+                return null;
+            }
+            return result;
+        }
+        return null;
+    }
+
 }
